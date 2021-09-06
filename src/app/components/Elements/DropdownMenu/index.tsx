e9d5818de7bdcd4a4c516c024bb18c2b11da6de7
MrChris17 <bonayonchristian975@gmail.com>
2021-09-06 18:00:46 +0800

    Created Dropdown menu & Generate Questionaire for Exam and Review

---
+++ src/app/components/Elements/DropdownMenu/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export function UserDropdownMenu() {
+  return (
+    <StyledDropdownMenu>
+      <DropdownMenuLink>Logout</DropdownMenuLink>
+    </StyledDropdownMenu>
+  );
+}
+
+export function AdminDropdownMenu() {
+  return <></>;
+}
+
+const StyledDropdownMenu = styled.div`
+  padding: 1rem;
+  width: 10rem;
+  position: absolute;
+  top: 4rem;
+  background-color: #fff;
+  border-radius: 0.375rem;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+`;
+
+const DropdownMenuLink = styled.a`
+  font-weight: 700;
+  cursor: pointer;
+
+  &:hover {
+    color: #4338ca;
+  }
+`;
